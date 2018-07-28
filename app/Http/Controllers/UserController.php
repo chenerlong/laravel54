22442f10c65197c8bb016dd35d66ef664660b205
chenerlong <chenerlong5420@163.com>
2018-07-28 21:42:13 +0800

    add

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -19,4 +20,44 @@ class UserController extends Controller
 
     }
 
+    //个人中心
+    public function show(User $user)
+    {
+        //个人信息，关注，粉丝，文章数
+        $user = User::withCount(['stars', 'fans', 'posts'])->find($user->id);
+
+        //文章列表，创建时间前十条
+        $posts = $user->posts()->orderBy('created_at', 'desc')->take(10)->get();
+
+        //关注用户，关注用户的关注数，粉丝数，文章数
+        $stars = $user->stars;
+        $susers = User::whereIn('id', $stars->pluck('star_id'))->withCount(['stars', 'fans', 'posts'])->get();
+
+        //粉丝用户，粉丝用户的关注数，粉丝数，文章数
+        $fans = $user->fans;
+        $fusers = User::whereIn('id', $fans->pluck('fan_id'))->withCount(['stars', 'fans', 'posts'])->get();
+
+
+        return view('user/show', compact('user', 'posts', 'susers', 'fusers'));
+    }
+
+    public function fan(User $user)
+    {
+        $me = \Auth::user();
+        \App\Fan::firstOrCreate(['fan_id' => $me->id, 'star_id' => $user->id]);
+        return [
+            'error' => 0,
+            'msg' => 'ok',
+        ];
+    }
+
+    public function unfan(User $user)
+    {
+        $me = \Auth::user();
+        \App\Fan::where('fan_id', $me->id)->where('star_id', $user->id)->delete();
+        return [
+            'error' => 0,
+            'msg' => 'ok',
+        ];
+    }
 }
--- app/User.php
+++ app/User.php
@@ -26,4 +26,51 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    //文章列表
+    public function posts()
+    {
+        return $this->hasMany(\App\Post::class, 'user_id', 'id');
+    }
+
+    //关注我的fan模型
+    public function fans()
+    {
+        return $this->hasMany(\App\Fan::class, 'star_id', 'id');
+    }
+    
+    //我关注的fan模型
+    public function stars()
+    {
+        return $this->hasMany(\App\Fan::class, 'fan_id', 'id');
+    }
+
+    //关注某人
+    public function doFan($uid)
+    {
+        $f = new \App\Fan();
+        $f->star_id = $uid;
+        return $this->stars()->save($f);
+    }
+
+    //取消关注某人
+    public function doUnfan($uid)
+    {
+        $f = new \App\Fan();
+        $f->star_id = $uid;
+        return $this->stars()->delete($f);
+    }
+
+    //当前用户是否被uid关注了
+    public function hasFun($uid)
+    {
+        return $this->fans()->where('fan_id', $uid)->count();
+    }
+
+    //当前用户是否关注了uid
+    public function hasStar($uid)
+    {
+        return $this->stars()->where('star_id', $uid)->count();
+    }
+
 }
